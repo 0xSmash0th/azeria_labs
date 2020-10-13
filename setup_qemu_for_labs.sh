2e69b964d44f1b6af822c7060914fbe899f96f02
Nigel Ploof <ploofn@gmail.com>
2020-10-13 14:36:16 -0700

    added lab setup script

---
+++ setup_qemu_for_labs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget http://downloads.raspberrypi.org/raspbian/images/raspbian-2017-04-10/2017-04-10-raspbian-jessie.zip
+git clone https://github.com/dhruvvyas90/qemu-rpi-kernel.git
+mkdir ~/qemu_vms/
