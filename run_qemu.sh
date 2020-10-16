be42dc2ec8d42d4c8ac6f61eef05c1b6097d1003
Nigel Ploof <ploofn@gmail.com>
2020-10-16 14:53:27 -0700

    added tap option for advanced networking

--- run_qemu.sh
+++ run_qemu.sh
@@ -3,12 +3,16 @@
 RASPBIAN=2017-04-10-raspbian-jessie
 set -x
 
+qemu_tap_cmd='sudo qemu-system-arm -kernel ~/qemu_vms/qemu-rpi-kernel/kernel-qemu-4.4.34-jessie -cpu arm1176 -m 256 -M versatilepb -serial stdio -append "root=/dev/sda2 rootfstype=ext4 rw" -hda ~/qemu_vms/${RASPBIAN}.img -net nic -net tap,ifname=tap0,script=no,downscript=no -no-reboot'
+qemu_ssh_cmd='qemu-system-arm -kernel ~/qemu_vms/qemu-rpi-kernel/kernel-qemu-4.4.34-jessie -cpu arm1176 -m 256 -M versatilepb -serial stdio -append "root=/dev/sda2 rootfstype=ext4 rw" -hda ~/qemu_vms/${RASPBIAN}.img -redir tcp:5022::22 -no-reboot'
+
 function error_check {
 	test $? -eq 0 || { echo "the last command failed"; exit 1; }
 }
 
 #check that we have qemu
 command -v qemu-system-arm >/dev/null 2>&1 || { echo >&2 "Please install qemu-system-arm.  Aborting."; exit 1; }
+command -v tunctl >/dev/null 2>&1 || { echo >&2 "Please install uml-utilities.  Aborting."; exit 1; }
 
 if [ $1 = "setup" ]; then 
 	
@@ -56,9 +60,19 @@ if [ $1 = "setup" ]; then
 	cd ~
 	error_check
 
+	# setup networking
+	sudo tunctl -t tap0 -u azeria
+	sudo ifconfig tap0 172.16.0.1/24
+
 	echo "########################################################\n\tRUN THE FOLLOWING COMMANDS IN THE RASPBIAN TERMINAL:\n\tsudo service ssh start\n\tsudo update-rc.d ssh enable\n\nYou should now be able to ssh to your qemu raspbian with: ssh pi@127.0.0.1 -p 5022\nThe default password is raspberry\n\nSee: https://azeria-labs.com/emulate-raspberry-pi-with-qemu/ for Advanced setup and trouble-shooting\n\n"
 	
-	qemu-system-arm -kernel ~/qemu_vms/qemu-rpi-kernel/kernel-qemu-4.4.34-jessie -cpu arm1176 -m 256 -M versatilepb -serial stdio -append "root=/dev/sda2 rootfstype=ext4 rw" -hda ~/qemu_vms/${RASPBIAN}.img -redir tcp:5022::22 -no-reboot
+	eval $qemu_ssh_cmd
+	
 else
-	qemu-system-arm -kernel ~/qemu_vms/qemu-rpi-kernel/kernel-qemu-4.4.34-jessie -cpu arm1176 -m 256 -M versatilepb -serial stdio -append "root=/dev/sda2 rootfstype=ext4 rw" -hda ~/qemu_vms/${RASPBIAN}.img -redir tcp:5022::22 -no-reboot
+	# setup ssh on port 5022 unless the tap cmd is present 
+	if [ $1 = "tap" ]; then
+		eval $qemu_tap_cmd
+	else
+		eval $qemu_ssh_cmd
+	fi
 fi
